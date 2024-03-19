0e2f5119dd0c550597fe23d1ebdb3068421e81fc
Ngubs <dumisane.ngubs578@gmail.com>
2024-03-19 20:47:58 +0200

    Created a new eKota app

---
+++ eKotaApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eKotaApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
