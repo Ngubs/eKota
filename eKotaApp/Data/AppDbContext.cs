b48f09b12187cfdcb02ac86ba36c0dafa1ef520e
Ngubs <dumisane.ngubs578@gmail.com>
2024-03-20 13:12:35 +0200

    Configure EF Core and DB created.

---
+++ eKotaApp/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+
+namespace eKotaApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { } 
+    }
+}
--- eKotaApp/Program.cs
+++ eKotaApp/Program.cs
@@ -1,7 +1,13 @@
+using eKotaApp.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseSqlServer(builder.Configuration.GetConnectionString("DB_Connection")));
 
 var app = builder.Build();
 
